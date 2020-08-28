96d8941c8f0ae5632004fd6887c48a6c54c8c9e6
Yisena Tiina <Yisena@outlook.com>
2020-08-28 19:37:22 +0800

    Revert "YisenaLib1.6"
    This reverts commit c0f9e2bfad374929a3b780c7975a39018f11e320.

--- YisenaLib.cpp
+++ YisenaLib.cpp
@@ -150,9 +150,11 @@ inline bool FreeWinodwHeap(HWND hwnd)
 //消息循环
 int MessageLoop()
 {
+
     MSG msg;
     while (GetMessage(&msg, nullptr, 0, 0))
     {
+        TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
     return (int)msg.wParam;
